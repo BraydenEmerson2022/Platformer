8569124706fad67179deeec2b5e48fb24e0f4b77
BraydenEmerson2022 <817275@mystma.org>
2021-01-11 13:30:31 -0600

    Added physics, camera, and movement
    Added physics, camera, and movement

---
+++ Assets/pl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pl : MonoBehaviour
+{
+    public float jumpPower = 1;
+
+    public float moveSpeed;
+
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpPower);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
+        }
+    }
+}
